18e50590b2b8f88d3f7ff6bb2ec98e9c3b2424fc
Matt Rigdon <mrigdon@salesforce.com>
2019-09-22 19:42:09 -0700

    init commit (it works)

---
+++ spec/lwc-create-component-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LwcCreateComponentView from '../lib/lwc-create-component-view';
+
+describe('LwcCreateComponentView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
